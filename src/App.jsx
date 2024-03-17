7c590c4f2b428b7e990e2a7d72d948bcfd57eb87
jpdoshi <jpdoshi2811@gmail.com>
2024-03-17 20:28:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+    <section className="hero">
+      <h1>Hi, I am Jainam.</h1>
+    </section>
+    <div className="hamburger-menu">
+      <div className="fries-wrapper">
+        <div className="fries"></div>
+        <div className="fries"></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
