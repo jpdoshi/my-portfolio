57b6612a8618e80e6d2ca6f9c26d7a432e70934f
jpdoshi <jpdoshi2811@gmail.com>
2024-03-18 15:23:47 +0530

    fast and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import $ from 'jquery'
 
 const App = () => {
-  let handleMenu = () => {
-    if (window.scrollX == 0) {
-      window.scrollTo($('main').width(), window.scrollY);
-      $('.menu').fadeIn(1200);
-      $('.close-icon').fadeIn(400);
-      $('.menu-icon').fadeOut(0);
-    } else {
-      window.scrollTo(0, window.scrollY);
-      $('.menu').fadeOut(400);
-      $('.close-icon').fadeOut(400);
-      $('.menu-icon').fadeIn(1200);
-    }
+  let openMenu = () => {
+    $('body').css('overflow-y', 'hidden');
+    $('.menu-icon').fadeOut(400);
+    $('.close-icon').fadeIn(400);
+    $('.side-menu').animate({left: '0%'}, 400);
+    $('.menu').delay(400).fadeIn(200);
+  }
+
+  let closeMenu = () => {
+    $('body').css('overflow-y', 'auto');
+    $('.menu-icon').fadeIn(400);
+    $('.close-icon').fadeOut(400);
+    $('.side-menu').animate({left: '100%'}, 400);
+    $('.menu').fadeOut(400);
   }
   return (
     <main>
     <div className="content">
+      <section className='hero'>
+        <div className='container'>
+          <h1>Hi, I am <span className='subject-title'>Jainam.</span></h1>
+          <p>
+          I am CS & IT student and enthusiast, who loves to build innovative projects 💡. I believe in self-learning and have fond of research and development in field of CS and IT. I enjoy designing UI/UX as well as business logic, algorithms and data related stuff 🚀. Feel free to refer to my github projects.
+          </p>
+          <a className='btn' href='#projects'>Explore Projects<FontAwesomeIcon style={{marginLeft: '12px'}} icon={faArrowRight}/></a>
+        </div>
+      </section>
+
+      <section className='hero'>
+        <div className='container'>
+          <h1>Hi, I am <span className='subject-title'>Jainam.</span></h1>
+          <p>
+          I am CS & IT student and enthusiast, who loves to build innovative projects 💡. I believe in self-learning and have fond of research and development in field of CS and IT. I enjoy designing UI/UX as well as business logic, algorithms and data related stuff 🚀. Feel free to refer to my github projects.
+          </p>
+          <a className='btn' href='#projects'>Explore Projects<FontAwesomeIcon style={{marginLeft: '12px'}} icon={faArrowRight}/></a>
+        </div>
+      </section>
+
       <section className='hero'>
         <div className='container'>
           <h1>Hi, I am <span className='subject-title'>Jainam.</span></h1>
@@ -34,10 +56,8 @@ const App = () => {
         <h1>Nav Menu</h1>
       </div>
     </div>
-    <div className='hamburger-menu'>
-      <FontAwesomeIcon icon={faBars} className='menu-icon' onClick={() => handleMenu()} />
-      <FontAwesomeIcon icon={faClose} className='close-icon' onClick={() => handleMenu()} />
-    </div>
+    <FontAwesomeIcon icon={faClose} className='close-icon' onClick={() => closeMenu()} />
+    <FontAwesomeIcon icon={faBars} className='menu-icon' onClick={() => openMenu()} />
     </main>
   )
 }
