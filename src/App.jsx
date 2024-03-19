0ab66609f92e046af9733e33a1e7662c5967208f
jpdoshi <jpdoshi2811@gmail.com>
2024-03-19 17:24:16 +0530

    changed design

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faBars, faClose } from '@fortawesome/free-solid-svg-icons'
+import { faBarsStaggered, faClose, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import $ from 'jquery'
 
 const App = () => {
   let openMenu = () => {
     $('body').css('overflow-y', 'hidden');
-    $('.menu-icon').fadeOut(400);
-    $('.close-icon').fadeIn(400);
-    $('.side-menu').animate({left: '0%'}, 400);
-    $('.menu').delay(400).fadeIn(200);
+    $('.menu-icon').fadeOut(250);
+    $('.close-icon').fadeIn(500);
+    $('.side-menu').animate({left: '0%'}, 250);
+    $('.menu').delay(300).fadeIn(400);
   }
 
   let closeMenu = () => {
     $('body').css('overflow-y', 'auto');
-    $('.menu-icon').fadeIn(400);
-    $('.close-icon').fadeOut(400);
-    $('.side-menu').animate({left: '100%'}, 400);
-    $('.menu').fadeOut(400);
+    $('.menu-icon').fadeIn(500);
+    $('.close-icon').fadeOut(250);
+    $('.side-menu').animate({left: '100%'}, 250);
+    $('.menu').fadeOut(250);
   }
   return (
     <main>
     <div className="content">
       <section className='hero'>
-        <div className='center'>
-          <h1>Hi, I am <span className='subject-title'>Jainam.</span></h1>
-          <p>
-          I am CS & IT student and enthusiast, who loves to build innovative projects 💡. I believe in self-learning and have fond of research and development in field of CS and IT. I enjoy designing UI/UX as well as business logic, algorithms and data related stuff 🚀. Feel free to refer to my github projects.
-          </p>
-          <ul>
-            <li><a href='https://in.linkedin.com/in/jainam-doshi-24063a2a0' style={{color: '#1976d2'}}><FontAwesomeIcon icon={faLinkedin} /></a></li>
-            <li><a href='https://github.com/jpdoshi' style={{color: 'black'}}><FontAwesomeIcon icon={faGithub} /></a></li>
-            <li><a href='https://www.instagram.com/_jpdoshi/' style={{color: '#e91e63'}}><FontAwesomeIcon icon={faInstagram} /></a></li>
-          </ul>
-        </div>
+        <h1>Hi, I'm <span>Jainam.</span></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam debitis exercitationem accusamus soluta, voluptatem explicabo nisi rerum in at architecto iusto repellendus eaque dolor dignissimos consectetur reiciendis odio quam dolores. Ab dolor deserunt, quas earum corporis vel ratione, nostrum incidunt consequuntur enim excepturi inventore placeat accusantium eius dolore veritatis alias!</p>
       </section>
     </div>
     <div className="side-menu">
@@ -44,10 +35,16 @@ const App = () => {
           <li><a href='#about'>ABOUT</a></li>
           <li><a href='#contact'>CONTACT</a></li>
         </ul>
+        <ul className='outer-links'>
+            <li><a href='https://in.linkedin.com/in/jainam-doshi-24063a2a0'><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            <li><a href='https://github.com/jpdoshi'><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a href='mailto:jpdoshi2811'><FontAwesomeIcon icon={faEnvelope} /></a></li>
+            <li><a href='https://instagram.com/_jpdoshi'><FontAwesomeIcon icon={faInstagram} /></a></li>
+          </ul>
       </div>
     </div>
     <FontAwesomeIcon icon={faClose} className='close-icon' onClick={() => closeMenu()} />
-    <FontAwesomeIcon icon={faBars} className='menu-icon' onClick={() => openMenu()} />
+    <FontAwesomeIcon icon={faBarsStaggered} className='menu-icon' onClick={() => openMenu()} />
     </main>
   )
 }
