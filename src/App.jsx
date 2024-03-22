705c50ec5c9bfdb3f5854742301cb30f23b9657a
jpdoshi <jpdoshi2811@gmail.com>
2024-03-22 14:29:09 +0530

    fixed performance bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ import Typewriter from 'typewriter-effect';
 const App = () => {
   let openMenu = () => {
     $('body').css('overflow-y', 'hidden');
-    $('.side-menu').animate({left: '0%'}, 250);
+    $('.side-menu').animate({ left: '0%' }, 250);
     $('.menu').delay(250).fadeIn(500);
 
-    $('.fries').eq(1).css({'opacity': '0', 'transform': 'translateX(1rem)'});
-    $('.fries').eq(0).css({'transform': 'translateY(9px) rotate(45deg)'});
-    $('.fries').eq(2).css({'transform': 'translateY(-9px) rotate(-45deg)'});
+    $('.fries').eq(1).css({ 'opacity': '0', 'transform': 'translateX(1rem)' });
+    $('.fries').eq(0).css({ 'transform': 'translateY(9px) rotate(45deg)' });
+    $('.fries').eq(2).css({ 'transform': 'translateY(-9px) rotate(-45deg)' });
   }
 
   let closeMenu = () => {
     $('body').css('overflow-y', 'auto');
-    $('.side-menu').animate({left: '100%'}, 250);
+    $('.side-menu').animate({ left: '100%' }, 250);
     $('.menu').fadeOut(250);
 
-    $('.fries').eq(1).css({'opacity': '1', 'transform': 'none'});
-    $('.fries').eq(0).css({'transform': 'none'});
-    $('.fries').eq(2).css({'transform': 'none'});
+    $('.fries').eq(1).css({ 'opacity': '1', 'transform': 'none' });
+    $('.fries').eq(0).css({ 'transform': 'none' });
+    $('.fries').eq(2).css({ 'transform': 'none' });
   }
 
   let handleMenu = () => {
@@ -34,16 +34,15 @@ const App = () => {
   }
   return (
     <main>
-    <div className="content">
-      <section className='hero' id='home'>
-        <div className='hero-grid'>
-          <div>
-            <img src='hero.gif'/>
-          </div>
-          <div>
-            <h1>Hey,<br/> I'm <Typewriter component={'span'}
-              onInit={(typewriter) => {
-                setInterval(() => {
+      <div className="content">
+        <section className='hero' id='home'>
+          <div className='hero-grid'>
+            <div>
+              <img src='hero.gif' />
+            </div>
+            <div>
+              <h1>Hey,<br /> I'm <Typewriter component={'span'}
+                onInit={(typewriter) => {
                   typewriter.typeString("Jainam.")
                     .pauseFor(800)
                     .deleteAll()
@@ -53,36 +52,37 @@ const App = () => {
                     .typeString("From India.")
                     .pauseFor(800)
                     .deleteAll()
+                    .pauseFor(800)
+                    .typeString("Jainam.")
                     .start();
-                }, 200);
-              }}/></h1>
-            <a href='#projects' className='btn'>Explore Projects<FontAwesomeIcon style={{marginLeft: '8px'}} icon={faArrowRight} /></a>
+                }} /></h1>
+              <a href='#projects' className='btn'>Explore Projects<FontAwesomeIcon style={{ marginLeft: '8px' }} icon={faArrowRight} /></a>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
-    <div className="side-menu">
-      <div className="menu">
-        <ul>
-          <li><a href='#home'>HOME</a></li>
-          <li><a href='#projects'>PROJECTS</a></li>
-          <li><a href='#about'>ABOUT</a></li>
-          <li><a href='#contact'>CONTACT</a></li>
-        </ul>
-        <ul className='outer-links'>
+        </section>
+      </div>
+      <div className="side-menu">
+        <div className="menu">
+          <ul>
+            <li><a href='#home'>HOME</a></li>
+            <li><a href='#projects'>PROJECTS</a></li>
+            <li><a href='#about'>ABOUT</a></li>
+            <li><a href='#contact'>CONTACT</a></li>
+          </ul>
+          <ul className='outer-links'>
             <li><a href='https://in.linkedin.com/in/jainam-doshi-24063a2a0'><FontAwesomeIcon icon={faLinkedin} /></a></li>
             <li><a href='https://github.com/jpdoshi'><FontAwesomeIcon icon={faGithub} /></a></li>
             <li><a href='mailto:jpdoshi2811'><FontAwesomeIcon icon={faEnvelope} /></a></li>
             <li><a href='https://instagram.com/_jpdoshi'><FontAwesomeIcon icon={faInstagram} /></a></li>
           </ul>
+        </div>
       </div>
-    </div>
-    <div className='nav-icon'><a href='/'>jpdoshi.</a></div>
-    <div className='menu-icon' onClick={() => handleMenu()}>
-      <div className='fries'></div>
-      <div className='fries'></div>
-      <div className='fries'></div>
-    </div>
+      <div className='nav-icon'><a href='/'>jpdoshi.</a></div>
+      <div className='menu-icon' onClick={() => handleMenu()}>
+        <div className='fries'></div>
+        <div className='fries'></div>
+        <div className='fries'></div>
+      </div>
     </main>
   )
 }
