4ed98a87c67f3a4f47b188e9616b2afb88d2f29b
jpdoshi <jpdoshi2811@gmail.com>
2024-08-13 15:39:44 +0530

    sections done

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,29 @@ import ResumeSection from './components/ResumeSection'
 import LinksSection from './components/LinksSection'
 
 const App = () => {
+  window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY;
+
+    const sections = document.getElementsByTagName('section');
+    const navLinks = document.getElementsByClassName('nav-link');
+
+    for (const current in sections) {
+      const sectionHeight = sections[current].offsetHeight;
+      const sectionTop = sections[current].offsetTop - 50;
+
+      if (
+        scrollY > sectionTop &&
+        scrollY <= sectionTop + sectionHeight
+      ) {
+        navLinks[current].className = "nav-link active";
+      } else {
+        navLinks[current].className = "nav-link";
+      }
+    };
+  });
+
   return (
-    <div style={{ display: 'flex' }}>
+    <div className='container'>
       <NavMenu />
       <div className='content'>
         <IntroSection />
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -6,14 +6,18 @@ const NavMenu = () => {
       <div style={{ height: '15vh' }}></div>
       <img src="https://avatars.githubusercontent.com/u/122164427?v=4" className='pfp' alt="Profile Image" />
       <div style={{ height: '16px' }}></div>
-      <a href='https://github.com/jpdoshi' style={{ marginLeft: '40px', color: '#ddd' }}>JPDOSHI</a>
+      <a href='https://github.com/jpdoshi' style={{
+        marginLeft: '50px', textTransform: 'lowercase',
+        color: '#ddd', fontFamily: 'Inter',
+        fontWeight: 'normal'
+      }}>jpdoshi.</a>
       <div style={{ height: '64px' }}></div>
       <ul>
-        <li><a href="#intro">INTRO</a></li>
-        <li><a href="#about">ABOUT ME</a></li>
-        <li><a href="#projects">PROJECTS</a></li>
-        <li><a href="#resume">RESUME</a></li>
-        <li><a href="#links">MORE</a></li>
+        <li><a href="#intro" className='nav-link active'>Intro</a></li>
+        <li><a href="#about" className='nav-link'>About Me</a></li>
+        <li><a href="#projects" className='nav-link'>Projects</a></li>
+        <li><a href="#resume" className='nav-link'>Resume</a></li>
+        <li><a href="#links" className='nav-link'>More</a></li>
       </ul>
     </nav>
   )
