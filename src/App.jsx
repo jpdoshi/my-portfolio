55318e34db567cfa1b919e37d147d470aeb57c01
jpdoshi <jpdoshi2811@gmail.com>
2024-09-25 13:29:06 +0530

    added chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,17 @@ const App = () => {
           <FooterSection />
         </div>
       </div>
+      <img
+        src='/assets/chatbot.png'
+        alt='chatbot'
+        height={120}
+        width={120}
+        loading='lazy'
+        onClick={() => {
+          window.open('https://ai.jpdoshi.tech/', '_blank');
+        }}
+        className='chatbot'
+      />
     </Suspense>
   )
 }
